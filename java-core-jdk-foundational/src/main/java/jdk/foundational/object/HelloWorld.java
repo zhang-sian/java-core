c6d26cebb543aa72d822b54fea1b670a32ae174f
zhangwm <644833165@qq.com>
2022-04-01 16:52:28 +0800

    First Code

---
+++ java-core-jdk-foundational/src/main/java/jdk/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package jdk.foundational.object;
+
+/**
+ * 我的第一个Java应用程序
+ *
+ * @author zhangwm 644833165@qq.com
+ * @version 20222022/4/1 16:39
+ * @since JDK8
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
